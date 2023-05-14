71b9777761ccb19fa6470485510d4106935d5e09
Rohit Patil <91825947+rohitspatil30@users.noreply.github.com>
2023-05-14 10:10:08 +0530

    Add files via upload

---
+++ user_input/scripts.js
@@ -0,0 +1,67 @@
+// user input : 
+// let firstName = window.prompt("give your first name : ");
+// let lastName = window.prompt("give your last name : ");
+// console.log(firstName, lastName);
+
+// let Name = console.prompt("what is your name : ");
+// console.log(Name);
+// =================RUNNING===============
+/*
+let username;
+document.getElementById("myButton").onclick = function () {
+    username = document.getElementById("myText").value;
+    console.log("Your name is : " + username);
+    document.getElementById("myLabel").innerHTML = "your name is: " + username;
+}
+*/
+// =================RUNNING===============
+
+/*
+ button clicking process is named under the function
+ where the id  and .onclick will make the the function run.
+
+ we store the value entered in the text box in username variable.
+
+ then print the value on console.
+
+ innerhtml will print the value in html page.
+
+*/
+// ====================================================
+// type conversion : 
+// =================RUNNING===============
+/*
+let age = window.prompt("how old are you");
+console.log(typeof age);
+console.log(age + 1);//the age is string now : 211 as the string is concatenated not added.
+
+age = Number(age);
+console.log(age + 1); //22
+console.log(typeof age);
+
+// boolean the string  if empty then false else true
+// for number the 0 is else true
+
+let bool = window.prompt("enter any string to check if it is true or false : ");
+
+console.log(typeof bool);
+console.log(bool);
+bool = Boolean(bool);
+console.log(bool);
+bool = Number(bool);
+console.log(bool);
+bool = Boolean(bool);
+console.log(bool);
+*/
+// =================RUNNING===============
+//const keyword 
+
+// const d = 56;
+// d += 1;
+// console.log(d); //not possible.
+
+// =================RUNNING===============
+
+
+
+
